0f3482ad75c2022069b65404744f2b14b8591f4e
BenR32 <44396287+BenR32@users.noreply.github.com>
2019-04-28 21:36:01 +0200

    Update first_steps.py

--- first_steps.py
+++ first_steps.py
@@ -2,21 +2,36 @@
 # Answers to questions that ask for non-code answers can simply be added as comments.
 
 # 1) The following line causes a SyntaxError. Please correct the line so that it's output is 'Hallo Welt!' [1P]
-print(Hallo Welt!)
+print('Hallo Welt!')
 
 # 2) Calculate the difference between a and b and assign the result to a variable called 'v_1'. What are the datatypes of a, b and 'v_1'? [2P]
 a = 976.543
 b = 345
+v_1 = a-b
 
+print(type(a))
+print(type(b))
+print(type(v_1))
 # 3) Calculate the remainder of the division 100/17 by only using one operator and save the result in v_2 (look into the Python docs for help) [1P]
+v_2 = 100 % 17
 
 # 4) The speed of light is about 300'000 km/s. Calculate the wavelength (in nanometers) of a light wave with a frequency of 5.0E+14 per second. Can we see this light? [4P]
 #    Save the result in v_3.
+speed_of_light = 300000
+frequency = 5.0E+14
+v_3 = (speed_of_light / frequency)*10.0E+12
+v_3 = int(v_3)
+maximum = 690
+minimum = 380
+sichtbar = v_3 > minimum and v_3 < maximum
 
+print("Der Wellenlängenbereich ist sichtbar:")
+print(sichtbar)
 # 5) Print the following string to the console using the format-function and the variables 'n' and 'pi': "Pi rounded to the first 10 decimals is: 3.1415926536" [2P]
 n  = 10
 pi = 3.14159265358979323846264338
 
+print("Pi rounded to the first 10 decimals is: {0:11.10f}".format(pi))
 # ------------------------------------
 # - Don't change stuff below this line
 # ------------------------------------
